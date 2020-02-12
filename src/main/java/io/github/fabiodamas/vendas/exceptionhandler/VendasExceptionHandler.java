ec6b714d981099e6fb7765d3e30c7728ca6dcd93
fabio damas <fabio.damas@gmail.com>
2020-02-11 23:38:02 -0300

    Atualizar apenas um campo

--- src/main/java/io/github/fabiodamas/vendas/exceptionhandler/VendasExceptionHandler.java
+++ src/main/java/io/github/fabiodamas/vendas/exceptionhandler/VendasExceptionHandler.java
@@ -19,69 +19,69 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @ControllerAdvice
 public class VendasExceptionHandler extends ResponseEntityExceptionHandler {
+		@Autowired
+		private MessageSource messageSource;
 
-	@Autowired
-	private MessageSource messageSource;
+		@Override
+		protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+																	  HttpHeaders headers, HttpStatus status, WebRequest request) {
 
-	@Override
-	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
-																  HttpHeaders headers, HttpStatus status, WebRequest request) {
-
-		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
-		String mensagemDesenvolvedor = ex.getCause().toString();
-		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
-		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
-	}
-
-	@Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-																  HttpHeaders headers, HttpStatus status, WebRequest request) {
-
-		List<Erro> erros = criarListaDeErros(ex.getBindingResult());
-		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
-	}
-
-	@ExceptionHandler({ EmptyResultDataAccessException.class })
-	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
-		String mensagemUsuario = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
-		String mensagemDesenvolvedor = ex.toString();
-		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
-		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
-	}
-
-	private List<Erro> criarListaDeErros(BindingResult bindingResult) {
-		List<Erro> erros = new ArrayList<>();
-
-		for (FieldError fieldError : bindingResult.getFieldErrors()) {
-			String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
-			String mensagemDesenvolvedor = fieldError.toString();
-			erros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+			String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
+			String mensagemDesenvolvedor = Optional.ofNullable(ex.getCause()).orElse(ex).toString();
+			List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+			return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
 		}
 
-		return erros;
+		@Override
+		protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+																	  HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+			List<Erro> erros = criarListaDeErros(ex.getBindingResult());
+			return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+		}
+
+		@ExceptionHandler({ EmptyResultDataAccessException.class })
+		public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
+			String mensagemUsuario = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+			String mensagemDesenvolvedor = ex.toString();
+			List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+			return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+		}
+
+		private List<Erro> criarListaDeErros(BindingResult bindingResult) {
+			List<Erro> erros = new ArrayList<>();
+
+			for (FieldError fieldError : bindingResult.getFieldErrors()) {
+				String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+				String mensagemDesenvolvedor = fieldError.toString();
+				erros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+			}
+
+			return erros;
+		}
+
+		public static class Erro {
+
+			private String mensagemUsuario;
+			private String mensagemDesenvolvedor;
+
+			public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
+				this.mensagemUsuario = mensagemUsuario;
+				this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+			}
+
+			public String getMensagemUsuario() {
+				return mensagemUsuario;
+			}
+
+			public String getMensagemDesenvolvedor() {
+				return mensagemDesenvolvedor;
+			}
+
+		}
+
 	}
-
-	public static class Erro {
-
-		private String mensagemUsuario;
-		private String mensagemDesenvolvedor;
-
-		public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
-			this.mensagemUsuario = mensagemUsuario;
-			this.mensagemDesenvolvedor = mensagemDesenvolvedor;
-		}
-
-		public String getMensagemUsuario() {
-			return mensagemUsuario;
-		}
-
-		public String getMensagemDesenvolvedor() {
-			return mensagemDesenvolvedor;
-		}
-
-	}
-
-}
--- src/main/java/io/github/fabiodamas/vendas/service/PessoaService.java
+++ src/main/java/io/github/fabiodamas/vendas/service/PessoaService.java
@@ -9,18 +9,34 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PessoaService {
-	
-	@Autowired
 	private PessoaRepository pessoaRepository;
 
-	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+	@Autowired
+	public PessoaService(PessoaRepository pessoaRepository) {
+		this.pessoaRepository = pessoaRepository;
+	}
 
-		Pessoa pessoaSalva = this.pessoaRepository.findById(codigo)
-				.orElseThrow(() -> new EmptyResultDataAccessException(1));
+
+	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
 
 		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
-
-		return this.pessoaRepository.save(pessoaSalva);
+		return pessoaRepository.save(pessoaSalva);
 	}
-	
+
+	public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		pessoaSalva.setAtivo(ativo);
+		pessoaRepository.save(pessoaSalva);
+	}
+
+	private Pessoa buscarPessoaPeloCodigo(Long codigo) {
+		Pessoa pessoaSalva = pessoaRepository.findById(codigo).orElse(null);
+
+		if (pessoaSalva == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		return pessoaSalva;
+	}
+
 }
